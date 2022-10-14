3fb2098bf0d684ec7b582942b202d00f712d21b7
사재빈 <sajaebin@sajaebin-ui-MacBookPro.local>
2022-10-14 17:24:24 +0900

    first

--- src/index.js
+++ src/index.js
@@ -5,13 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Library from './first_proj/Library';
+import Clock from './clock_proj/clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>
+  );
+}, 1);  
+
+// root.render(
+//   <React.StrictMode>
+//     <Clock />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
